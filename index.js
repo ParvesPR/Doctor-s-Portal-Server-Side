a4285cc3fd5357f84b5de98be74feb16f807775f
ParvesPR <parves2585@gmail.com>
2022-05-16 13:31:59 +0800

    create api for showing available appointment

--- index.js
+++ index.js
@@ -30,7 +30,34 @@ async function run() {
 
         });
 
-        // RECEIVE BOOKING APPOINTMENT DATA
+        // GET AVAILABLE APPOINTMENT
+        app.get('/available', async (req, res) => {
+            const date = req.query.date;
+
+            // step 1:  get all services
+            const services = await doctorCollection.find().toArray();
+
+            // step 2: get the booking of that day. output: [{}, {}, {}, {}, {}, {}]
+            const query = { date: date };
+            const bookings = await bookingCollection.find(query).toArray();
+
+            // step 3: for each service
+            services.forEach(service => {
+                // step 4: find bookings for that service. output: [{}, {}, {}, {}]
+                const serviceBookings = bookings.filter(book => book.treatment === service.name);
+                // step 5: select slots for the service Bookings: ['', '', '', '']
+                const bookedSlots = serviceBookings.map(book => book.slot);
+                // step 6: select those slots that are not in bookedSlots
+                const available = service.slots.filter(slot => !bookedSlots.includes(slot));
+                //step 7: set available to slots to make it easier 
+                service.slots = available;
+            });
+
+
+            res.send(services);
+        })
+
+        // RECEIVE BOOKING APPOINTMENT DATA & LIMIT APPOINTMENT
         app.post('/appointment', async (req, res) => {
             const booking = req.body;
             const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient };
