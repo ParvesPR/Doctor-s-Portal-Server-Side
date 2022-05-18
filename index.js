d082dbd2a849d2e4ab61366e1693fe5bcc212c9e
ParvesPR <parves2585@gmail.com>
2022-05-18 14:28:29 +0800

    verify jwt token

--- index.js
+++ index.js
@@ -14,7 +14,22 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yg9f1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-console.log('DB Connected')
+console.log('DB Connected');
+
+function verifyJwt(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
 
 async function run() {
     try {
@@ -74,13 +89,19 @@ async function run() {
         });
 
         // GET A SINGLE USER DATA
-        app.get('/appointments', async (req, res) => {
+        app.get('/appointments', verifyJwt, async (req, res) => {
             const patient = req.query.patient;
-            const authorization = req.headers.authorization;
-            console.log(authorization);
-            const query = { patient: patient };
-            const bookings = await bookingCollection.find(query).toArray();
-            res.send(bookings);
+            const decodedEmail = req.decoded.email;
+            if (patient === decodedEmail) {
+                const query = { patient: patient };
+                const bookings = await bookingCollection.find(query).toArray();
+                return res.send(bookings);
+            }
+            else {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+
+
         })
 
         // RECEIVE BOOKING APPOINTMENT DATA & LIMIT APPOINTMENT
