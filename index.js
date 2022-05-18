450766050695c9c435dd1c30aa36408016261f6a
ParvesPR <parves2585@gmail.com>
2022-05-19 06:48:31 +0800

    find admin user only

--- index.js
+++ index.js
@@ -47,9 +47,35 @@ async function run() {
 
         });
         // LOAD ALL USERS
-        app.get('/allusers', async (req, res) => {
+        app.get('/user', verifyJwt, async (req, res) => {
             const users = await userCollection.find().toArray();
             res.send(users);
+        });
+
+        // MAKE USER AN ADMIN
+        app.put('/user/admin/:email', verifyJwt, async (req, res) => {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                };
+                const result = await userCollection.updateOne(filter, updateDoc);
+                res.send(result);
+            }
+            else {
+                res.status(403).send('forbidden')
+            }
+        });
+
+        // FIND ADMIN USER ONLY
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
         });
 
         // GET USERS
